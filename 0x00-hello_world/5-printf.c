1acb3ab62647ca46cea3adb8abc4540086ce5da0
khawla_ben <refeser52@gmail.com>
2023-09-07 11:06:04 +0000

    ddf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - debut
+ *
+ *Return: la valeur 0 to quit
+ *
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
