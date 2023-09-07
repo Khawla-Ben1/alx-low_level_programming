6648d56210322ce0c289ce5c827b7e3ce2c2af54
khawla_ben <refeser52@gmail.com>
2023-09-07 11:00:22 +0000

    ddf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - debut
+ * @void: sans arguments
+ * Description: le programme est commence et finie ici . .
+ * Return: la valeur 0 est pour exiter
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
