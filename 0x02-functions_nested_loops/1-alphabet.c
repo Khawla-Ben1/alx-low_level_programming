8569f4cfa21259ab6af1a89d19e7221e08c6e588
khawla_ben <refeser52@gmail.com>
2023-09-12 11:00:59 +0000

    final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,16 @@
 
 
 /**
- *main - Debut
- *Return: zero
+ *main - start
+ *Return: always 0
+ *Description : the function prints the alphabet in lowercase
  */
 void print_alphabet(void)
 {
 	int i;
+
 	i = 'a';
-	while(i < 'z')
+	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
