502888f46b079f9b5fddba60cfb0b501747330d4
khawla_ben <refeser52@gmail.com>
2023-09-25 02:20:27 +0000

    push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * _strcat - 
+ * _strcat - concat 2 string
  * @dest:char
  * @src:char
- * Return:char pointeur
+ * Return:char
  */
 char *_strcat(char *dest, char *src)
 {
+	char *s = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
