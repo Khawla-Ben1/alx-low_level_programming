3c38aca7523b85a8ce6f1435b5599ea3ecc6c8e3
khawla_ben <refeser52@gmail.com>
2023-10-15 15:34:00 +0000

    reopos variadic functions done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+*sum_them_all - sums all the parameters
+*@n: first parameter
+*
+*Return: 0 if n is null
+* or sum of parameters in other case
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list l;
+	int sum = 0;
+	unsigned int i;
+
+	va_start(l, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		sum += va_arg(l, int);
+	}
+	va_end(l);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - prints numbers followed by new line
+*@separator: string to be printed between numbers
+*@n: number of integer to be passed
+*
+*Return: nothing
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list l;
+	unsigned int i;
+
+	va_start(l, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(l, int));
+		if (i != (n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(l);
+}
