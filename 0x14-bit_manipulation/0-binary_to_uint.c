82b5018775489f4fe0f0888c627b1f3e17b59053
khawla_ben <refeser52@gmail.com>
2024-02-08 21:45:10 +0000

    updates in  bit-manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,27 @@
 #include "main.h"
+
 /**
- * binary_to_uint - converts to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
+ * binary_to_uint - function to convert binary to unsigned integer
+ * @b: binary number
+ * Return: 0 or converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int d = 0;
-    int len = 0, base = 1;
+	unsigned int result = 0;
+	int a = 0, c = 0;
 
-    if (!check_valid_string(b))
-    return (0);
-    while (b[len] != '\0')
-        len++;
-    while (len)
-    {
-        d += ((b[len - 1] - '0') * base);
-        base *= 2;
-        len--;
-    }
-    return (d);
-}
-
-/**
- * check_valid_string - checks if a string has only 0 and 1
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-    if (b == NULL)
-    return (0);
-    while (*b)
-    {
-        if (*b != '1' && *b != '0')
-        return (0);
-        b++;
-    }
-    return (1);
+	if (b == NULL || (*b != '0' && *b != '1'))
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+		;
+	a--;
+	while (a >= 0)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+		result = result + (b[a] - '0') * (1 << c);
+		a--;
+		c++;
+	}
+	return (result);
 }
