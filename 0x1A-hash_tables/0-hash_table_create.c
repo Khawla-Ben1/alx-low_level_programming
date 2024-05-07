0ec49c3341ac118dbee2c3e3fe2b36667cc7d064
khawla_ben <refeser52@gmail.com>
2024-05-07 14:22:09 +0000

    check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
-
 	if (ht == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
@@ -24,7 +22,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-
 	for (; i < size; i++)
 		ht->array[i] = NULL;
 
