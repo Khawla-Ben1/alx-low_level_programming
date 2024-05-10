0ad20cc29a77a1fa6b02b1b73617ec009020062b
khawla_ben <refeser52@gmail.com>
2024-05-10 12:21:43 +0000

    updates

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_memcpy - copies n bytes from memory area src to memory area dest. 
+*_memcpy - copies n bytes from memory area src to memory area dest.
 *@dest:area where bytes are copied to
 *@src:area where bytes are copied from
 *@n:number of bytes to copy
